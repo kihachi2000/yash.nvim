5a57093ba88ae93a0a5cb3c391fc7bcc85cd464e
kihachi2000 <kenbayashi.gaming.00@gmail.com>
2023-06-20 15:52:17 +0900

    feat: add highlights for lsp-semantic-highlight

--- lua/yash/themes/lsp.lua
+++ lua/yash/themes/lsp.lua
@@ -22,4 +22,20 @@ M.DiagnosticLineNrWarn = { fg = colors.light_gray, bg = colors.warn_bg, bold = t
 M.DiagnosticLineNrInfo = { fg = colors.light_gray, bg = colors.info_bg, bold = true }
 M.DiagnosticLineNrHint = { fg = colors.light_gray, bg = colors.hint_bg, bold = true }
 
+M["@lsp.type.class"] = { fg = colors.dark_green }
+M["@lsp.type.decorator"] = { fg = colors.dark_gray }
+M["@lsp.type.enum"] = { fg = colors.dark_green }
+M["@lsp.type.enumMember"] = { fg = colors.light_gray }
+M["@lsp.type.function"] = { fg = colors.light_green }
+M["@lsp.type.interface"] = { fg = colors.dark_green }
+M["@lsp.type.macro"] = { fg = colors.blue }
+M["@lsp.type.method"] = { fg = colors.light_green }
+M["@lsp.type.namespace"] = { fg = colors.light_gray }
+M["@lsp.type.parameter"] = { fg = colors.fg }
+M["@lsp.type.property"] = { fg = colors.fg }
+M["@lsp.type.struct"] = { fg = colors.dark_green }
+M["@lsp.type.type"] = { fg = colors.dark_green }
+M["@lsp.type.typeParameter"] = { fg = colors.light_gray }
+M["@lsp.type.variable"] = { fg = colors.blue }
+
 return M
