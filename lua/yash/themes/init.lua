f568f83294ccc24ee26ff69196b99c5e465b9f04
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2023-07-26 09:13:49 +0900

    feat: add yash.themes.language module

--- lua/yash/themes/init.lua
+++ lua/yash/themes/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 M.editor = require("yash.themes.editor")
+M.language = require("yash.themes.language")
 M.lsp = require("yash.themes.lsp")
 M.markdown = require("yash.themes.markdown")
 M.plugins = require("yash.themes.plugins")
---
+++ lua/yash/themes/language.lua
@@ -0,0 +1,8 @@
+local colors = require("yash.colors")
+
+local M = {}
+
+-- vim
+M.vimFunction = { fg = colors.pink, bold = true }
+
+return M
